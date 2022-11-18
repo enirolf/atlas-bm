dd73608d8a40459f52153711082bca37585c36cb
Florine de Geus <git@fwdg.nl>
2022-11-18 08:50:01 +0100

    Add docker setup

--- src/compression/bm_compression.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-DAOD_PHYS_DIR="/cvmfs/atlas.cern.ch/repo/tutorials/asg/cern-nov2022/"
-DAOD_PHYS_FILE="$DAOD_PHYS_DIR/DAOD_PHYS.30350968._000001.pool.root.1"
--- src/compression/gen_ttree.cxx
+++ src/compression/gen_ttree.cxx
@@ -9,28 +9,40 @@
 #include <TTree.h>
 
 #include <iostream>
+#include <string>
 
 #include <unistd.h>
 
-int main(int argc, char **argv)
-{
-  std::string inputPath = "";
-
-  int c;
-
-  while((c = getopt(argc, argv, "i")) != -1) {
-    switch(c) {
-    case 'i':
-      inputPath = optarg;
-      break;
-    default:
-      std::cerr << "Unknown option -" << c << std::endl;
-      return 1;
-    }
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    std::cerr << "No file specified, exiting..." << std::endl;
+    return 1;
   }
 
-  std::cout << "Hello, world!" << std::endl;
-  // TFile &iputFile(TFile::Open(inputPath));
+  std::string inputPath = argv[1];
+
+  std::cout << inputPath << std::endl;
+  TFile *inputFile = TFile::Open(inputPath.c_str());
+
+  if (!inputFile) {
+    delete inputFile;
+    return 1;
+  }
+
+  std::cout << "Total xAOD size (in bytes): \t\t" << inputFile->GetSize()
+            << std::endl;
+
+  TTree *collectionTree = inputFile->Get<TTree>("CollectionTree");
+
+  std::cout << "CollectionTree total size (in bytes): \t"
+            << collectionTree->GetTotBytes() << std::endl;
+
+  std::cout << "CollectionTree zip size (in bytes): \t"
+            << collectionTree->GetZipBytes() << std::endl;
+
+  TFile *outputFile = TFile::Open("data/collection_ttree~0.root", "RECREATE",
+                                  "CollectionTree", 0);
+  outputFile->WriteObject(collectionTree, "CollectionTree");
 
   return 0;
 }
